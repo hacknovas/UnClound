16a355b2d3f09df021914aa04377f731b5dfef38
hacknovas <creatives.doni@gmail.com>
2024-06-10 17:39:26 +0530

    Remove cached .env files

--- Backend/test/test_Uncloud.js
+++ Backend/test/test_Uncloud.js
@@ -1,31 +1,21 @@
 const { expect } = require("chai");
-// const { ethers } = require("ethers");
 
 describe("Uncloud", async () => {
-  it("Basic Check", async () => {
+  it("All Checks", async () => {
     const [owner, second, third] = await ethers.getSigners();
-    console.log(owner.address);
-
+    console.log("\nOwner Address: ", owner.address);
     const instance = await ethers.deployContract("UnCloud");
-
-    const createT = await instance.storeMetaData("sdgjgskahkshffdka", "Mahi");
-    // console.log(tokenID3.returnValue);
-
+    const createT = await instance.storeMetaData("Data", "Mahi", "key");
     const token = await instance._metaID();
-    console.log(token);
-
+    console.log("\nToken ID: ", token);
     const mynft = await instance.getMyData();
-    console.log(mynft);
-
+    console.log("\nMy Meta Data", mynft);
     const shareNFT = await instance.shareDataWith(second.address, 1);
-
     const getAlladdrr = await instance.connect(owner).getAllAddress(1);
-    console.log(getAlladdrr);
-
+    console.log("\nShared Address: ", getAlladdrr);
     const viewNFT = await instance.connect(second).viewMetaData(1);
-    console.log(viewNFT);
-
+    console.log("\nGet MetaData Using Second Address: ", viewNFT);
     const getAllNFT = await instance.connect(second).getMySharedData();
-    console.log(getAllNFT);
+    console.log("\nGet All MetaData: ", getAllNFT);
   });
 });
